1b90ddc45e4cbe2063b277bc051d23d76ff33677
mrjoshuagordon <mr.joshuagordon@gmail.com>
2014-03-08 14:39:44 -0800

    Formatting

--- ui.R
+++ ui.R
@@ -5,25 +5,26 @@ shinyUI(bootstrapPage(
   
   sidebarPanel(
     
+    # X Value Input
     sliderInput(inputId = "xinput", 
                 label = "X Value", 
                 min=1, max=9, value=7),
     
+    # Y Value Input
     sliderInput(inputId = "yinput", 
                 label = "Y Value", 
                 min=1, max=9, value=7),
     
-    textInput("pch", "Caption:", "") ,
+    # Text Input
+    textInput("pch", "Caption:", "Hello World") ,
     
+    # Color Drop Down    
     selectInput(inputId = "colors",
                 label = "Select Color",
                 input.colors)
-    
-    
-  )
+    ),
   
-  ,
-  
+  # Plot it 
   mainPanel(div(align = "center",
                 plotOutput(outputId = "main_plot", height = "500px", width="600px"))
   )
